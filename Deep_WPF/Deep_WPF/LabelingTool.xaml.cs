f31f618b9d8a0ecad03aaedbd2b3e6e5d1f48b86
Bkook95-Semics <bkook95@gmail.com>
2019-08-21 21:08:08 +0900

    기본틀 잡고 레이블링 툴 기능 작업중

---
+++ Deep_WPF/Deep_WPF/LabelingTool.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Winforms = System.Windows.Forms;
+using System.IO;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace Deep_WPF
+{
+    /// <summary>
+    /// LabelingTool.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LabelingTool : System.Windows.Window
+    {
+        string folderPath;
+
+        public LabelingTool()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_InputDir_Click(object sender, RoutedEventArgs e)
+        {
+            Winforms.FolderBrowserDialog folderDlg = new Winforms.FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = false;
+            folderDlg.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            Winforms.DialogResult result = folderDlg.ShowDialog();
+
+            if(result == Winforms.DialogResult.OK)
+            {
+                lb_filelist.Items.Clear();
+                tb_InputDir.Text = folderPath = folderDlg.SelectedPath;
+                string[] filePaths = Directory.GetFiles(folderDlg.SelectedPath, "*.jpg");
+                foreach(var name in filePaths)
+                {
+                    string[] split = name.Split('\\');
+                    lb_filelist.Items.Add(split[split.Length-1]);
+                }
+            }
+
+        }
+
+        private void lb_filelist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var bigImageMat = new Mat(folderPath + "\\" + lb_filelist.SelectedItem.ToString());
+            Im_Image.Source = bigImageMat.ToWriteableBitmap();
+        }
+
+
+        Boolean isDragging = false;
+        System.Windows.Point ptStart;
+        private void Im_Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(isDragging == false)
+            {
+                ptStart = e.GetPosition(Im_Image);
+                isDragging = true;
+            }
+        }
+
+        private void Im_Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(isDragging)
+            {
+                isDragging = false;
+            }
+        }
+
+        private void Im_Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (isDragging)
+            {
+                double x = e.GetPosition(Im_Image).X;
+                double y = e.GetPosition(Im_Image).Y;
+                
+                recSelection.Margin = new Thickness(ptStart.X+5, ptStart.Y+5, ptStart.Y+5, ptStart.X+5);
+                recSelection.Width = Abs(ptStart.X - x);
+                recSelection.Height = Abs(ptStart.Y - y);
+                
+                if (recSelection.Visibility != Visibility.Visible)
+                {
+                    recSelection.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        public static double Abs(double number)
+        {
+            return (number > 0) ? number : -number; 
+        }
+    }
+}
--- Deep_WPF/Deep_WPF/MainWindow.xaml.cs
+++ Deep_WPF/Deep_WPF/MainWindow.xaml.cs
@@ -24,5 +24,23 @@ namespace Deep_WPF
         {
             InitializeComponent();
         }
+
+        private void btn_labeling_Click(object sender, RoutedEventArgs e)
+        {
+            Window Labeling = new Labeling();
+            Labeling.Show();
+        }
+
+        private void btn_train_Click(object sender, RoutedEventArgs e)
+        {
+            Window Test = new Test();
+            Test.Show();
+        }
+
+        private void btn_test_Click(object sender, RoutedEventArgs e)
+        {
+            Window Train = new Train();
+            Train.Show();
+        }
     }
 }
